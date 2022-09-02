5bf503addc296e93a647c7ad1c9f7367771a19b0
Gabriel Xavier <gxavier4@its.jnj.com>
2022-09-02 12:15:38 -0300

    Solving swiftlint issues

--- Sources/Model/Fruit.swift
+++ Sources/Model/Fruit.swift
@@ -1,19 +1,19 @@
 import Foundation
 
 public struct Fruit: Decodable {
-    
+
     public var id: Int
-    
+
     public var name: String
-    
+
     public var genus: String
-    
+
     public var family: String
-    
+
     public var order: String
-    
+
     public var nutritions: NutritionInformation
-    
+
     public init(id: Int, name: String, genus: String, family: String, order: String, nutritions: NutritionInformation) {
         self.id = id
         self.name = name
@@ -22,5 +22,5 @@ public struct Fruit: Decodable {
         self.order = order
         self.nutritions = nutritions
     }
-    
+
 }
